fde99528e8eeea255423d9b73c0dfae3fa50ce2b
Numan Razzaq <numanrazzaq24@gmail.com>
2021-05-20 00:55:11 -0700

    login page complete

--- projects/demo-app/src/app/use/login/login.component.ts
+++ projects/demo-app/src/app/use/login/login.component.ts
@@ -2,38 +2,32 @@ import { Component, OnInit, Output, EventEmitter, Inject } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { Router } from '@angular/router'
 import { DialogComponent } from '../dialog/dialog.component';
-import {CookieService} from 'ngx-cookie-service';
+import { CookieService } from 'ngx-cookie-service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
- 
-
-  public remember:boolean=false;
-
-  email="";
-  password="";
- 
-   constructor(public dialog: MatDialog, private router: Router, 
-    public cookiesservice:CookieService  
-  ) {}
-   ngOnInit(): void { 
-   }
-   
- dologin(){
-  if( this.email=="numanrazzaq24@gmail.com" && this.password == "numan24"){
-    this.router.navigateByUrl('/first')}
-    else {
-      alert("Enter Valid Details.")
-    }
+  email = "";
+  password = "";
   
- }
+  constructor(public dialog: MatDialog, private router: Router,
+    public cookiesservice: CookieService
+  ) { }
+  ngOnInit(): void {
+  }
+
+  dologin() {
+    if(this.email == "ali@gmail.com" && this.password == "numan"){
+     this.router.navigateByUrl('/first')}
+    else {
+      alert("Enter Valid Details.")}
+  };
   opendialog() {
     this.dialog.open(DialogComponent)
   }
-  clickme(){
-    
+  clickme() {
+
   }
 }
